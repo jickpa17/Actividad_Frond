0be4f7cf44c3292a6fd1395e5763b826b18c67dc
jickpa17 <jickpa17@hotmail.com>
2024-05-17 19:56:37 -0500

    Correccion del .js y se agrega otra pelicula

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 // JavaScript para filtrar películas
 function filtrarPeliculas(categoria) {
-    let tarjetas = document.querySelectorAll('.card');
-    tarjetas.forEach(tarjeta => {
-      let categoriaTarjeta = tarjeta.querySelector('.card-text').textContent;
-      if (categoria === 'Todas' || categoriaTarjeta.includes(categoria)) {
-        tarjeta.style.display = 'block';
+  let tarjetas = document.querySelectorAll('.card');
+  tarjetas.forEach(tarjeta => {
+      let categoriaTarjeta = tarjeta.querySelector('.card-body p.' + categoria);
+      if (categoria === 'Todas' || categoriaTarjeta) {
+          tarjeta.style.display = 'block';
       } else {
-        tarjeta.style.display = 'none';
+          tarjeta.style.display = 'none';
       }
-    });
-  }
+  });
+}
+
+
+
   
